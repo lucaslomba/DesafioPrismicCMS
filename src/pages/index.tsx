c451d2944beabe2907ff93e8f69723ddb36ae7b3
lucaslomba <lucasazevedolomba@gmail.com>
2021-10-18 23:11:35 -0300

    Iniciando desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +25,56 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }) {
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <img src="/Logo.svg" alt="Logo" />
+        {posts.map(post => (
+          <a href="">
+            <strong>{post.data.title}</strong>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <span>{post.first_publication_date}</span>
+              <span>{post.data.author}</span>
+            </div>
+          </a>
+        ))}
+        
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </main>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 5,
+  });
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return{
+      slug: post.uid,
+      first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle, 
+        author: post.data.author
+      }
+    }
+  })
+
+  return {
+    props:{
+      posts
+    }
+  }
+};
